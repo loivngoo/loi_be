f422d10996483fa8609167c99d7c133f0b11b279
Saabirose <hathaiviet411@gmail.com>
2023-05-26 21:59:07 +0700

    [Upload

--- api/web.js
+++ api/web.js
@@ -1,10 +1,12 @@
 import userRoute from '../routes/users.routes';
 import adminRoute from '../routes/admin.routes';
 import agentRoute from '../routes/agent.routes';
+import uploadRoute from '../routes/upload.routes';
 const initWebRouter = (app) => {
     userRoute(app);
     adminRoute(app);
     agentRoute(app);
+    uploadRoute(app);
 };
 
 export default initWebRouter;
---
+++ controllers/Chat/upload.js
@@ -0,0 +1,18 @@
+const uploadImage = async(req,res) => {
+  const file = req.file
+  if (!file) {
+    const error = new Error('Please upload a file')
+    error.httpStatusCode = 400
+    return next(error)
+  }
+  // res.send()
+  return res.status(200).json({
+    status: 200,
+    message: 'Đăng ký thành công',
+    path: process.env.APP_PATH + "/upload/images/" + req.body.fileName
+});
+}
+
+module.exports = {
+  uploadImage
+};
